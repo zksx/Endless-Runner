d4e793be0258f87a918a62bc11f3262b7b34645f
Nicholas Persley <nsp73@nau.edu>
2023-11-09 16:59:41 -0700

    Remove prefabs in scene
    - Maps now spawn instead of automatically show up (hard-set in scene)
    - Commented out previous spawning code and currently spawn the first three maps
    - Note: Map placements/connecting will be added later

--- Assets/Scripts/MapSpawner.cs
+++ Assets/Scripts/MapSpawner.cs
@@ -8,36 +8,37 @@ using System.Collections.Generic;
 public class MapSpawner : MonoBehaviour
 {
     public GameObject prevMap;
-    public GameObject[] Maps;
-
+    public List<GameObject> Maps;
+    public List<GameObject> createdMaps;
 
     void Start()
     {
         Vector3 mapSpawn = Vector3.zero;
-        Maps = null;
         prevMap = null;
 
-        if (Maps == null)
-            Maps = GameObject.FindGameObjectsWithTag("Map");
+        //foreach (GameObject Map in Maps)
+        //{
+        //    // check if pervmap isn't null
+        //    if (prevMap != null)
+        //    {
 
+        //        // find end postion of prev map
+        //        mapSpawn = prevMap.transform.position + (Vector3.forward * 10f);
 
+        //    }
+
+        //    print(mapSpawn);
+
+        //    Instantiate(Map, mapSpawn,  Map.transform.rotation);
+
+        //    prevMap = Map;
+        //}
+
+        // Create the first three maps
+        // TODO: Figure out coordinate offsets to connect maps
         foreach (GameObject Map in Maps)
         {
-            // check if pervmap isn't null
-            if (prevMap != null)
-            {
-
-                // find end postion of prev map
-                mapSpawn = prevMap.transform.position + (Vector3.forward * 10f);
-
-            }
-
-            print(mapSpawn);
-
-
-            Instantiate(Map, mapSpawn,  Map.transform.rotation);
-
-            prevMap = Map;
+            createdMaps.Add(Instantiate(Map));
         }
     }
 
