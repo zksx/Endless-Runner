9eefc54d698c3dca645e0b9cca412dc39ace3d39
Nicholas Persley <nsp73@nau.edu>
2023-11-14 13:10:36 -0700

    Add terrain generation
    - Game now spawns new maps, randomly from the possible prefabs
    - New maps will be spawned when the distance from the player to the last map is small enough
    - Delete the first map on the road when the player is far enough away from it

--- Assets/Scripts/MapSpawner.cs
+++ Assets/Scripts/MapSpawner.cs
@@ -10,45 +10,76 @@ public class MapSpawner : MonoBehaviour
     public GameObject prevMap;
     public List<GameObject> Maps;
     public List<GameObject> createdMaps;
+    public int numCreatedMaps;
+    public int numMapPrefabs;
+
+    public GameObject ballObject;
 
     void Start()
     {
         Vector3 mapSpawn = Vector3.zero;
         prevMap = null;
-
-        //foreach (GameObject Map in Maps)
-        //{
-        //    // check if pervmap isn't null
-        //    if (prevMap != null)
-        //    {
-
-        //        // find end postion of prev map
-        //        mapSpawn = prevMap.transform.position + (Vector3.forward * 10f);
-
-        //    }
-
-        //    print(mapSpawn);
-
-        //    Instantiate(Map, mapSpawn,  Map.transform.rotation);
-
-        //    prevMap = Map;
-        //}
+        numCreatedMaps = 0;
+        numMapPrefabs = 0;
+        ballObject = GameObject.Find("Player");
 
         // Create the first three maps
-        // TODO: Figure out coordinate offsets to connect maps
         foreach (GameObject Map in Maps)
         {
-            createdMaps.Add(Instantiate(Map));
+            GameObject mapSpawned = Instantiate(Map);
+            
+            PlaceMap(mapSpawned);
+
+            numMapPrefabs++;
         }
     }
 
     private void Update()
     {
-        
+        // Get positions of the player and the start/end maps that
+        // have been spawned
+        Vector3 ballPos = ballObject.transform.position;
+        Vector3 farthestPos = createdMaps[numCreatedMaps - 1].transform.position;
+        Vector3 ballToEndDist = farthestPos - ballPos;
+
+        Vector3 startPlatPos = createdMaps[0].transform.position;
+        Vector3 startToBallDist = ballPos - startPlatPos;
+
+        // Check to see if the distance from the player to the farthest
+        // platform ahead is not far enough
+        if (ballToEndDist.magnitude < 400f)
+        {
+            // Create new random platform
+            int randMap = Random.Range(0, numMapPrefabs - 1);
+            GameObject appendedMap = Instantiate(Maps[randMap]);
+
+            PlaceMap(appendedMap);
+        }
+
+        // Check to see if the player is far enough away from the first
+        // platform
+        if(startToBallDist.magnitude > 100f)
+        {
+            // Deleted the platform
+            GameObject mapToRemove = createdMaps[0];
+            Destroy(mapToRemove);
+
+            createdMaps.RemoveAt(0);
+            numCreatedMaps--;
+        }
     }
 
-    private void PlaceMap(GameObject[] Maps)
+    private void PlaceMap(GameObject newMap)
     {
+        if(prevMap != null)
+        {
+            // Update the new position based on the previous platform
+            newMap.transform.position = prevMap.transform.position + (Vector3.forward * 50f);
+        }
 
+        createdMaps.Add(newMap);
+
+        prevMap = newMap;
+        numCreatedMaps++;
     }
 }
